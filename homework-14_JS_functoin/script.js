75d60f23f1b5bd166f72c2ec10681a79245d91ac
Semen84 <pulka03118@mail.ru>
2025-02-25 13:01:13 +1000

    Add files via upload

---
+++ homework-14_JS_functoin/script.js
@@ -0,0 +1,62 @@
+let numbers = [1, 2, 3, 4, 6];
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i] + 1);
+}
+
+/*Домашка*/
+/*Задача №1*/
+/*function nameHallo(name) {
+  return `Hello ${name}`;
+}
+console.log(nameHallo('Alex'));*/
+/*========================================*/
+/*Задача №2*/
+/*let arr = [5, 10, 13, 27, 2];
+function moreThan(numbers) {
+  for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 10) {
+      console.log(numbers[i]);
+    }
+  }
+  return "Все";
+}
+console.log(moreThan(arr));*/
+/*==========================================*/
+/*Задача №3*/
+/*function calculator(num1, num2, operator) {
+  switch(operator) {
+    case 'plus':
+      return num1 + num2;
+    case 'minus':
+      return num1 - num2;
+    case 'multiply':
+      return num1 * num2;
+    case 'divide':
+      if(num2 === 0) return 'Ошибка: деление на ноль!';
+      return num1 / num2;
+    default:
+      return 'Ошибка: неизвестная операция!';
+  }
+}
+let result = calculator(5,7,'divide');
+console.log(result);
+let result2 = calculator(5,7,'plus');
+console.log(result2);
+let result3 = calculator(5,7,'minus');
+console.log(result3);
+let result4 = calculator(5,7,'multiply');
+console.log(result4);*/
+
+/*function calculateNambers(num1, num2, operator) {
+  if (operator === "plus") {
+    return num1 + num2;
+  } else if (operator === "minus") {
+    return num1 - num2;
+  } else if (operator === "divide") {
+    return num1 / num2;
+  } else if (operator === "multiply") {
+    return num1 * num2;
+  }
+}
+let result = calculateNambers(5, 6, "multiply");
+console.log(result);*/
