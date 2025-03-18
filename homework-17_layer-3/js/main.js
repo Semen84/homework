f97dd8e0f84b8ec54e48142a945c849f965b729f
Semen84 <pulka03118@mail.ru>
2025-03-18 17:46:01 +1000

    Add files via upload

--- homework-17_layer-3/js/main.js
+++ homework-17_layer-3/js/main.js
@@ -6,15 +6,12 @@
     const burgerNavLink = e.target.closest(".nav__link");
 
     if (!burgerIcon && !burgerNavLink) return;
-    if(document.documentElement.clientWidth > 900) return;
-   
-if(!document.body.classList.contains('body--opened-menu')) {
-    document.body.classList.add('body--opened-menu')
-} else {
-    document.body.classList.remove('body--opened-menu')
-}
+    if (document.documentElement.clientWidth > 900) return;
 
-
+    if (!document.body.classList.contains("body--opened-menu")) {
+      document.body.classList.add("body--opened-menu");
+    } else {
+      document.body.classList.remove("body--opened-menu");
+    }
   }
-
 })();
