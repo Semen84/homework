02622e7fe34590d24a71012bc66f9cef76b9c24d
Semen84 <pulka03118@mail.ru>
2025-03-21 17:30:21 +1000

    Add files via upload

--- homework-17_layer-3/js/main.js
+++ homework-17_layer-3/js/main.js
@@ -15,3 +15,46 @@
     }
   }
 })();
+
+
+
+
+// Модальное окно
+const modal = document.querySelector('.modal');
+const openModalButton = document.querySelector('.about__img-button');
+const closeModalButton = document.querySelector('.modal__cancel');
+
+// Функции управления модалкой
+function openModal() {
+  document.body.classList.add('body--opened-modal');
+}
+
+function closeModal() {
+  document.body.classList.remove('body--opened-modal');
+}
+
+// Открытие по клику на кнопку
+openModalButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  openModal();
+});
+
+// Закрытие по клику на крестик
+closeModalButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  closeModal();
+});
+
+// Закрытие по клику вне модалки
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+// Закрытие по ESC
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && document.body.classList.contains('body--opened-modal')) {
+    closeModal();
+  }
+});
